e8973f07a06c1650c8ad9e4acb1b605efc82dcce
emilly.santos <emilly.santos@atitus.edu.br>
2026-06-16 20:53:37 -0300

    aula 16.06

--- src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
+++ src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
@@ -1,20 +1,25 @@
 package br.edu.atitus.apisample.controllers;
 
+import br.edu.atitus.apisample.dtos.SigninDTO;
 import br.edu.atitus.apisample.dtos.SignupDTO;
 import br.edu.atitus.apisample.entities.User;
 import br.edu.atitus.apisample.entities.UserType;
 import br.edu.atitus.apisample.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationConfiguration auth;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationConfiguration auth) {
         this.userService = userService;
+        this.auth = auth;
     }
 
     @PostMapping ("/signup")
@@ -24,6 +29,14 @@ public class AuthController {
         newUser.setType(UserType.Common);
         userService.save(newUser);
         return ResponseEntity.status(201).body(newUser);
+    }
+
+    @PostMapping ("/signin")
+    public ResponseEntity<String> postSignin(@RequestBody SigninDTO dto) throws Exception {
+        auth.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(dto.email(), dto.password()));
+
+        //TODO gerar e assinar o jwt válido
+                return ResponseEntity.ok("AQUI IRÁ O JWT (JSON WEB TOKEN)");
     }
 
     @ExceptionHandler(Exception.class)
--- src/main/java/br/edu/atitus/apisample/services/UserService.java
+++ src/main/java/br/edu/atitus/apisample/services/UserService.java
@@ -2,6 +2,9 @@ package br.edu.atitus.apisample.services;
 
 import br.edu.atitus.apisample.entities.User;
 import br.edu.atitus.apisample.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,7 @@ import org.springframework.stereotype.Service;
 // Ou seja, os objetos serão criados e gerenciados pelo Spring IOC
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository repository;
 
     private final PasswordEncoder encoder;
@@ -51,4 +54,10 @@ public class UserService {
         return repository.save(newUser);
 
 }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com este email!"));
+    }
 }
