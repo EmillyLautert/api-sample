218f4d6ed7da776ba150a62aa1da7100e2fbf7ce
emilly.santos <emilly.santos@atitus.edu.br>
2026-06-17 20:11:09 -0300

    aula 17.06

--- src/main/java/br/edu/atitus/apisample/configs/ConfigSecurity.java
+++ src/main/java/br/edu/atitus/apisample/configs/ConfigSecurity.java
@@ -1,5 +1,6 @@
 package br.edu.atitus.apisample.configs;
 
+import br.edu.atitus.apisample.components.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,17 +8,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ConfigSecurity {
+
     @Bean
-    public SecurityFilterChain getFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain getFilterChain(HttpSecurity http, AuthTokenFilter filterToken) throws Exception {
         http.sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/ws**", "/ws/**").authenticated()
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .addFilterBefore(filterToken, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -25,4 +29,5 @@ public class ConfigSecurity {
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
